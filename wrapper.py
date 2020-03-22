d80c119c0a3703916e359284d4dfbdf1ad8162d7
shubhamiitbhu <shubhamk.ojha.civ16@iitbhu.ac.in>
2020-03-22 22:25:41 +0530

    writing in xlsx

--- wrapper.py
+++ wrapper.py
@@ -4,25 +4,32 @@ import pandas
 import csv
 import re
 import scrapper
+import xlwt 
+from xlwt import Workbook 
 import json
+
 
 def getJson(filepath):
     with open (filepath, 'r') as fp:
         return json.load(fp)
 
+wb = Workbook() 
+sheet1 = wb.add_sheet('Sheet 1') 
 data = getJson('./stationCodetoName.json')
 data_len = len(data)
-results = {}
 dataframe = []
 len=0
 for key in data:
+    results = {}
     print("Scraping for " + key + " " + str(data_len-len) + " to go")
     train_details = scrapper.scraping(key)
     results[key]=train_details
-    len=len+1
-    
-    
+    sheet1.write(len, 0, key)
+    row_len=0
+    for i in train_details:
+        row_len +=1
+        sheet1.write(len, row_len, str({i :train_details[i]}))
 
-dataframe.append(results)
-df = pandas.DataFrame(dataframe)
-df.to_json("train_schedule.json")
+    len=len+1
+    wb.save('results.xls') 
+  
